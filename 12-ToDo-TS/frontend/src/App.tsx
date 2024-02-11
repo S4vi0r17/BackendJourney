517093d5a8707249b72f7b218da05a09204535f2
S4vi0r <gbp17@outlook.com>
2024-02-11 10:06:14 -0500

    Initi

---
+++ 12-ToDo-TS/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl">Hola, perras</h1>
+    </>
+  )
+}
+
+export default App
