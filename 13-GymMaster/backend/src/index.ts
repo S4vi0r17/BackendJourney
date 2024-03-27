88deb5f81c7b006e39d360d80526efc21df9e5d1
S4vi0r <gbp17@outlook.com>
2024-03-27 11:45:23 -0500

    Add middleware for logging, CORS, and parsing request body

--- 13-GymMaster/backend/src/index.ts
+++ 13-GymMaster/backend/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 import connectDB from './config/db'
 
 connectDB()
 
 const PORT = process.env.PORT || 4000
 const app = express()
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', (req, res) => {
     res.send('Hi!!!')
