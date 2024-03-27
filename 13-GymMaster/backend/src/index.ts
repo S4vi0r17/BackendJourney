d9f5e69359c411f9208ea5a086b6e19e1e83a4c9
S4vi0r <gbp17@outlook.com>
2024-03-27 12:14:38 -0500

    Add client routes

--- 13-GymMaster/backend/src/index.ts
+++ 13-GymMaster/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import clientRoutes from './routes/clientRoutes'
 import connectDB from './config/db'
 
 connectDB()
@@ -13,9 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', (req, res) => {
-    res.send('Hi!!!')
-})
+app.use('/api/clients', clientRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on: http://localhost:${PORT}/`);
