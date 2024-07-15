0a62b786444ba60a88b18779546c686112037db2
S4vi0r <gbp17@outlook.com>
2024-07-15 14:56:08 -0500

    refactor: Add login route for trainers

--- 13-GymMaster/backend/src/routes/trainer.route.ts
+++ 13-GymMaster/backend/src/routes/trainer.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { registration, profile, confirm } from '../controllers/trainer.controllers'
+import { registration, profile, confirm, auth } from '../controllers/trainer.controllers'
 
 const router = Router()
 
 router.post('/', registration)
 router.get('/profile', profile)
 router.get('/confirm/:token', confirm)
+router.post('/login', auth)
 
 export default router
