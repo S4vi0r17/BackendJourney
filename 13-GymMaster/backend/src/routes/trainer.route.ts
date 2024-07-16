e76a7ed0a33f92577d78909d9085be649a7fe5a5
S4vi0r <gbp17@outlook.com>
2024-07-15 22:10:58 -0500

    refactor: Update trainer route to include authentication middleware for profile endpoint

--- 13-GymMaster/backend/src/routes/trainer.route.ts
+++ 13-GymMaster/backend/src/routes/trainer.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { registration, profile, confirm, auth } from '../controllers/trainer.controllers'
+import checkAuth from '../middleware/auth.middleware'
 
 const router = Router()
 
@@ -7,6 +8,6 @@ router.post('/', registration)
 router.get('/confirm/:token', confirm)
 router.post('/login', auth)
 
-router.get('/profile', profile)
+router.get('/profile', checkAuth, profile)
 
 export default router
