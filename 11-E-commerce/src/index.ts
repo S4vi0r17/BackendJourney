57c4ada71ff1e5ef1d87b74303e28ebe5bf71ae7
S4vi0r <gbp17@outlook.com>
2024-07-20 12:14:11 -0500

    refactor: Update E-commerce package.json and dependencies

--- 11-E-commerce/src/index.ts
+++ 11-E-commerce/src/index.ts
@@ -1,21 +1,42 @@
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-const app: Express = express();
-const __dirname: string = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-let PORT: string | number = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-	res.render('index')
+  res.render('index', { title: 'E-commerce', message: 'Bienvenido a nuestro E-commerce' });
 });
 
-// Iniciar el servidor
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.listen(PORT, () => {
-	console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+/* 
+	import express, { Express, Request, Response } from 'express';
+	import path from 'path';
+	import { fileURLToPath } from 'url';
+
+	const app: Express = express();
+	const __dirname: string = path.dirname(fileURLToPath(import.meta.url));
+
+	let PORT: string | number = process.env.PORT || 3000;
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'pug');
+
+	app.use(express.static('public'));
+
+	app.get('/', (req, res) => {
+		res.render('index')
+	});
+
+	// Iniciar el servidor
+	app.listen(PORT, () => {
+		console.log(`Servidor escuchando en el puerto ${PORT}`);
+	});
+*/
